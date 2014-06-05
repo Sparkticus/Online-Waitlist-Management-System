6090e63bcd8841bf4dd5a75d5fd257ce6be826ec
Joanna Bi <jbi@wellesley.edu>
2014-06-05 16:28:47 -0400

    profhome commenting update
    forgot to update this one file on github

--- WW_ProfHome.java
+++ WW_ProfHome.java
@@ -19,58 +19,60 @@ import org.apache.commons.lang.*;
 public class WW_ProfHome extends HttpServlet {
 
     private void doRequest(HttpServletRequest req, HttpServletResponse res)
-      throws ServletException, IOException, SQLException
+  throws ServletException, IOException, SQLException
     {
   res.setContentType("text/html; charset=UTF-8");
   PrintWriter out = res.getWriter();
   String selfUrl = res.encodeURL(req.getRequestURI());
   HttpSession session = req.getSession(true);
-  //String sessId = session.getId();
   Connection con = null;
         
   printPageHeader(out,session);
         
   String session_bid = (String)session.getAttribute("session_bid");
   String session_type =(String)session.getAttribute("session_type");
-  //out.println(session_bid);
-  //out.println(session_type);
         
   if (session_bid == null || !session_type.equals("professor")){
-      out.println("Please log in or create an account.");
-      out.println("<a href='/walter/servlet/WW_Signin'>Click here to sign in</a>");
+      out.println("<div class='alert alert-danger'><strong>Sorry!</strong> To view this page you must log in or create an account. <a href='/walter/servlet/WW_Signin'>Click here to sign in</a>.</div>");
   } else {
       try {
     con = WalterDSN.connect("walter_db");
     
+    // Get request data
     String remove_bid = req.getParameter("remove_bid");
     String remove_waitlist = req.getParameter("remove_waitlist");
-    String current_crn =req.getParameter("view_waitlist");
-    
+    String current_crn =req.getParameter("view_waitlist");    
     String order = req.getParameter("new_order");
     String previous_crn = (String)session.getAttribute("session_crn");
     //String current_crn = req.getParameter("waitlist_id");
+
+    // Print out Professor stats
+    out.println("<div class='panel panel-primary'>"+
+          "<div class='panel-heading'><h3 class='panel-title'>Account Information</h3></div>"+
+          "<div class='panel-body'>");
+    out.println("Name: "+(String)session.getAttribute("session_name"));
+    out.println("<br>Banner ID: "+(String)session.getAttribute("session_bid"));
+    out.println("<br>Email: "+(String)session.getAttribute("session_email"));
+    out.println("<br>Department: "+(String)session.getAttribute("session_department"));
+    out.println("<br>Account Type: Professor");
+    out.println("</div></div>");
     
+    // Check buttons
     if (remove_bid!=null){
-        removeStudent(req, out, con,remove_bid,previous_crn);
+        removeStudent(req, out, con,remove_bid, previous_crn);
     }
     if (remove_waitlist!=null){
-        removeWaitlist(req, out, con,remove_waitlist);
+        removeWaitlist(req, out, con, remove_waitlist);
     }
     if (order!=null && previous_crn!=null){
         String [] sort = StringUtils.split(order,',');
-        processSort(req, out, con,sort,previous_crn);
+        processSort(req, out, con, sort, previous_crn);
         String not_using_auto_crn = getProfActivity(session_bid, out, con, selfUrl);
     } else {
         String auto_crn = getProfActivity(session_bid, out, con, selfUrl);
-        
-        //out.println("prof crn: "+auto_crn);
-        //out.println(previous_crn);
-        //out.println(current_crn);
-        
         if (current_crn==null){
       current_crn=auto_crn;
         }
-        
     }
     if (current_crn!=previous_crn) {
         if (StringUtils.length(current_crn)>3){
@@ -79,21 +81,12 @@ public class WW_ProfHome extends HttpServlet {
       session.setAttribute("session_crn",previous_crn);
         }
     }
-    
-    out.println("<div class='jumbotron'>"); //bootstrap css
-    out.println("<h2>View Waitlist</h2>");
-    Enumeration keys = session.getAttributeNames();
-    while (keys.hasMoreElements()) {
-        String key = (String)keys.nextElement();
-        out.println(key + ": " + session.getValue(key) + "<br>");
-    }
-    
+
     String waitlist_id = (String)session.getAttribute("session_crn");
-    //out.println("<br>crn used:"+waitlist_id);
-                
     if (waitlist_id!=null) {
         processForm(req, out, con, waitlist_id, selfUrl);
     }
+    
       }
       catch (SQLException e) {
     out.println("Error: "+e);
@@ -105,7 +98,7 @@ public class WW_ProfHome extends HttpServlet {
     close(con);
       }
   }
-  out.println("</div>"); // close jumbotron div
+  out.println("</div>"); //close row div
         out.println("<div class='footer'>");
         out.println("<p>&copy; Joanna Bi and Lindsey Tang 2014</p>");
         out.println("</div>");
@@ -134,14 +127,13 @@ public class WW_ProfHome extends HttpServlet {
     private String getProfActivity(String prof_bid, PrintWriter out, Connection con, String selfUrl)
   throws SQLException
     {
-        out.println("<div class='jumbotron'>"); //bootstrap css
-        out.println("<h2>Professor's Created Waitlists</h2>");
         PreparedStatement query_student = con.prepareStatement
       ("select * from Created_Waitlist where bid=?");
         query_student.setString(1, escape(prof_bid));
         ResultSet result_student = query_student.executeQuery();
-        //Int count = 0;
+
         String waitlist_id ="";
+  out.println("<div class=row><div class='col-sm-3'><div class='list-group'>");
         while (result_student.next()) {
             waitlist_id=result_student.getString("crn");
             PreparedStatement query_waitlist = con.prepareStatement
@@ -155,20 +147,12 @@ public class WW_ProfHome extends HttpServlet {
                 String course_limit = result_waitlist.getString("course_limit");
                 String kind = result_waitlist.getString("kind");
     // Print activity
-                out.println("waitlist_id: "+ waitlist_id);
-                out.println(" course_name: "+ course_name);
-                out.println(" course_num: "+ course_num);
-                out.println(" department: "+ department);
-                out.println(" course_limit: "+ course_limit);
-                out.println(" kind: "+ kind);
-                //out.println("<button onclick=remove_waitlist('"+waitlist_id+"')>Remove</button>");
-                out.println("<button onclick=view_waitlist('"+waitlist_id+"')>View</button>");
-                //out.println("<form method='post' action='"+selfUrl+"'><button type='submit' name='view_waitlist' value='"+waitlist_id+"'>View</button></form>");
-                out.println("<br>");
+    out.println("<a href='#' onclick=view_waitlist('"+waitlist_id+"') class='list-group-item'>");
+    out.println("<h4 class='list-group-item-heading''>"+course_num+" "+course_name+"</h4>");
+    out.println("<p class='list-group-item-text'>"+kind+" limit: "+course_limit+"</p></a>");
             }
         }
-        //out.println("<hr>");
-  out.println("</div>"); //closes jumbotron div
+  out.println("</div></div>"); //close panel div
         return waitlist_id;
     }
     
@@ -195,7 +179,7 @@ public class WW_ProfHome extends HttpServlet {
   query.setString(1, escape(waitlist_id) );
   query.setString(2, escape(remove_bid) );
         int result = query.executeUpdate();
-  out.println("Student Removed<br>");
+  out.println("<div class='alert alert-info'><strong>Student removed.</strong></div>");
     }
     
     // Delete a waitlist
@@ -206,7 +190,7 @@ public class WW_ProfHome extends HttpServlet {
       ("DELETE from Course where crn=?");
         query.setString(1, escape(waitlist_id) );
         int result = query.executeUpdate();
-        out.println("Waitlist Removed<br>");
+  out.println("<div class='alert alert-info'><strong>Waitlist removed.</strong></div>");
     }
     
     // Process sorting of students on waitlist
@@ -221,9 +205,13 @@ public class WW_ProfHome extends HttpServlet {
       query.setString(3, escape(sort[i]) );
       int result = query.executeUpdate();
   }
-        out.println("Order Saved!<br>");
+  out.println("<div class='alert alert-info'><strong>Order saved.</strong></div>");
     }
     
+    // ========================================================================
+    // CONTROL PANEL: PROCSSING THE FORM
+    // ========================================================================
+
     // Process the form
     private void processForm(HttpServletRequest req, PrintWriter out, Connection con, String waitlist_id, String selfUrl )
   throws SQLException
@@ -279,7 +267,6 @@ public class WW_ProfHome extends HttpServlet {
         out.println("<h3 class='text-muted'>Walter</h3>");
         out.println("</div>");
     }
-
     
     // Prints list of students on searched for waitlist
     private void printList(Connection con, PrintWriter out, String waitlist_id, String selfUrl)
@@ -287,18 +274,18 @@ public class WW_ProfHome extends HttpServlet {
     {
   try {
       Statement query2 = con.createStatement();
-        //out.println(waitlist_id);
       ResultSet result2 = query2.executeQuery("select * from Course where crn = "+waitlist_id);
       
+      out.println("<div class='col-sm-9'><div class='well'>");
       if (result2.next()) {
-    out.println("<p>"+result2.getString("course_num")+" "+result2.getString("course_name")+"<br>"+
-          result2.getString("kind")+" Limit: "+result2.getString("course_limit")+"<br>");
+    out.println("<p class='lead'>"+result2.getString("course_num")+" "+result2.getString("course_name")+"<br>"+
+          "CRN: "+waitlist_id+" "+result2.getString("kind")+" Limit: "+result2.getString("course_limit")+"<br></p>");
       }
       
       Statement query = con.createStatement();
       ResultSet result = query.executeQuery
     ("select * from Waitlist where waitlist_id = '"+waitlist_id+"' order by rank asc");
-      out.println("<ul id='students'>");
+      out.println("<ul id='students' style='list-style-type: none; padding-left:0;'>");
       while (result.next()) {
     String student_bid = result.getString("student_bid");
     String student_name = result.getString("student_name");
@@ -307,30 +294,32 @@ public class WW_ProfHome extends HttpServlet {
     String submitted_on  = result.getString("submitted_on");
     String rank = result.getString("rank");
     String explanation = result.getString("explanation");
+
+    // Print students on waitlist
     if(!result.wasNull()) {
-        out.println("<li id='"+student_bid+"'>"+rank+
-        " "+student_name+" "+major_minor+" "+student_class+" "+explanation+
-        "<button onclick=remove_student('"+student_bid+"')>Remove</button></li>");
+        out.println("<li id='"+student_bid+"' class='ui-state-default' style='background:transparent; border:none;'><b>"+rank+"</b>. "+student_name+", "+major_minor+", Class of "+student_class+"<br>Explanation: "+explanation+"<button style='position: absolute; right:40;' class='btn btn-xs btn-danger' onclick=remove_student('"+student_bid+"')>Remove</button></li>");
     } else {
         out.println("result was not null"); //when there's no result, print error statement
     }
       }
-      out.println("</ul>");
-      //out.println("<form method='post' action='/walter/servlet/WW_ProfHome'><button  type='submit' name='waitlist_id' value="+waitlist_id+">Start Over</button></form>");
-      out.println("<button onclick=start_over('"+waitlist_id+"')>Refresh Order</button>");
-      out.println("<button id='sort'>Save Order</button>");
-      out.println("<button id='email_button'>View Email List</button>");
-      out.println("<button onclick=remove_waitlist('"+waitlist_id+"')>Remove Waitlist</button><br>");
+      out.println("</ul><br>");
+      out.println("<button class='btn btn-default' onclick=start_over('"+waitlist_id+"')>Refresh Order</button>");
+      out.println("<button class='btn btn-default' id='sort'>Save Order</button>");
+      out.println("<button class='btn btn-default' id='email_button'>View Email List</button>");
+      out.println("<button class='btn btn-default' onclick=remove_waitlist('"+waitlist_id+"')>Remove Waitlist</button><br>");
       printScript(out, selfUrl);
+      out.println("</div></div>");
+
   } catch (SQLException e) {
       out.println("<p>Error: "+e);
   }
     }
     
     private void printScript(PrintWriter out, String selfUrl){
-        out.println("<script>$('#students').sortable();</script>");
+        out.println("<script>$('#students').sortable({placeholder: 'ui-state-highlight'});"+ //sortable is an outside function from jQuery UI library at 'jqueryui.com/sortable/' that provides functionality for click and drag list sorting
+                    "$('#students').disableSelection();</script>");
         out.println("<script>"+
-                    "function post_to_url(path, params, method) {"+
+                    "function post_to_url(path, params, method) {"+ //post_to_url is an outside function that submits a post request to an url, used as an event handler for onclick events, source: stackoverflow.com/questions/133925/javascript-post-request-like-a-form-submit
                     "method = method || 'post';"+
                     "var form = document.createElement('form');"+
                     "form.setAttribute('method', method);"+
@@ -349,9 +338,9 @@ public class WW_ProfHome extends HttpServlet {
                     "}");
         out.println("function remove_student(bid) {"+
                     "if (confirm('Are you sure you want to remove student from waitlist?')) {"+
-                        "post_to_url('"+selfUrl+"', {remove_bid:bid.toString()});"+
+        "post_to_url('"+selfUrl+"', {remove_bid:bid.toString()});"+
                     "} else {"+
-                        "}"+
+        "}"+
                     "};");
         out.println("function remove_waitlist(waitlist_id) {"+
                     "if (confirm('Are you sure you want to remove the waitlist?')) {"+
@@ -367,7 +356,7 @@ public class WW_ProfHome extends HttpServlet {
         "};");
         out.println("$(document).ready(function(){"+
                     "$('#sort').click(function(){"+
-        "var sorted = $( '#students' ).sortable( 'toArray' );"+
+        "var sorted = $( '#students' ).sortable( 'toArray' );"+//sortable is an outside function from jQuery UI library 'jqueryui.com/sortable/' that provides functionality for click and drag list sorting
         "post_to_url('"+selfUrl+"',{new_order:sorted});"+
         "});"+
                     "$('#email_button').click(function(){"+
@@ -384,7 +373,7 @@ public class WW_ProfHome extends HttpServlet {
   try {
       Statement query = con.createStatement();
       ResultSet result = query.executeQuery
-        ("select email from Person,Waitlist where Person.bid=Waitlist.student_bid and Waitlist.waitlist_id ="+waitlist_id);
+    ("select email from Person,Waitlist where Person.bid=Waitlist.student_bid and Waitlist.waitlist_id ="+waitlist_id);
       out.println("<div id='email_list' style='display: none;'> Students Emails:");
       while (result.next()) {
     out.println(result.getString("email")+";");
@@ -394,7 +383,7 @@ public class WW_ProfHome extends HttpServlet {
   catch (SQLException e) {
       out.println("<p>Error: "+e);
   }
-  }
+    }
     
     // ========================================================================
     // HELPER METHOD: ESCAPING
